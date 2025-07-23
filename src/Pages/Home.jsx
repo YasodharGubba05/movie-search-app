3ef3dc4c3cd750c1612067cfb1b0104a675a571c
YasodharGubba05 <yasodhargubba@gmail.com>
2025-07-23 21:25:24 +0530

    added styling

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,48 +1,56 @@
 import { useState } from "react";
+const API_KEY = "cc8e6713";
+import "../App.css"
 
-export default function Home() {
-  const [searchTerm, setsearchTerm] = useState("");
+function Home() {
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const API_KEY = "cc8e6713";
 
-  const handleSearch = async () => {
-    try {
-      const response = await fetch(
-        `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchTerm}`
-      );
-      const data = await response.json();
-      if (data.Search) {
-        setMovies(data.Search);
-      } else {
-        setMovies([]);
-      }
-    } catch (error) {
-      console.error("Error fetching movies:", error);
+  const searchMovies = async () => {
+    
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
+    );
+    const data = await response.json();
+    if (data.Search) {
+      setMovies(data.Search);
+    } else {
+      setMovies([]);
     }
   };
-    
-  return (
-    <div>
-      <div className="searchbar">
-        <input
-          type="text"
-          placeholder="enter movie name"
-          value={searchTerm}
-          onChange={(e) => setsearchTerm(e.target.value)}
-        />
-        <button onClick={handleSearch}>Submit</button>
-      </div>
 
-    
-      <div className="movie-container">
-        {movies.map((movie) => (
-          <div key={movie.imdbID}>
-            <h2>{movie.Title}</h2>
-            <p>{movie.Year}</p>
-            <img src={movie.Poster} alt={movie.Title} />
-          </div>
-        ))}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    searchMovies();
+  };
+
+  return (
+    <div className="home-container">
+      <form onSubmit={handleSubmit} className="search-form">
+        <input className="input"
+          type="text"
+          placeholder="Search for movies..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="searchbtn">Search</button>
+      </form>
+
+      <div className="movie-list">
+        {movies.length > 0 ? (
+          movies.map((movie) => (
+            <div className="movie-card" key={movie.imdbID}>
+              <img src={movie.Poster} alt={movie.Title} />
+              <h3>{movie.Title}</h3>
+              <p>{movie.Year}</p>
+            </div>
+          ))
+        ) : (
+          <p>No movies found</p>
+        )}
       </div>
     </div>
   );
 }
+
+export default Home;
