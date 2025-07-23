1e8e6117f3436800cd8599db597ef2864bbede61
YasodharGubba05 <yasodhargubba@gmail.com>
2025-07-23 11:18:34 +0530

    adding to the initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-
+import Home from "./Pages/Home"
+import Favourites from "./Pages/Favourites"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css"
 
 function App() {
 
   return (
     <>
+    <Router>
+      <header className="navbar">
+        <h1>🎬 Movie Explorer</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/favorites">Favorites</Link>
+        </nav>
+      </header>
+
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favourites />} />
+      </Routes>
+    </Router>
     </>
   )
 }
---
+++ src/Pages/Favourites.jsx
@@ -0,0 +1,7 @@
+export default function Favourites() {
+    return (
+        <div className="container"> 
+         <h2>Your Favourite Movies</h2>
+        </div>
+    )
+}
